68005fe717aeccfb91457cc94f40f4e0c57880bc
kevin knight <tgodkev@gmail.com>
2021-11-30 23:24:18 -0500

    added map func

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((notes)=>  
+      <Note 
+      key={notes.id}
+      title={notes.title}
+      content={notes.content}
+    />)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
-import App from './components/App';
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-ReactDOM.render(
-    <App />, document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-
-
-
-
-//1. Create a new React app.
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
-
-
-//firebase token.
-// 1//04zi2RCO99PaNCgYIARAAGAQSNwF-L9Ir8pHETpaudwFicTDd5ZjrNPU-4Wqy1b-3C4GcoQh84XEEs-Jh-11pWA5BgJ3W1OCwrR4
-
-//Example: firebase deploy --token "$FIREBASE_TOKEN"
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
